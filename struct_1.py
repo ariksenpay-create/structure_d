4d5bec2dc785450d89f93d9b42f824276b0bc79e
Kira <ariksenpay@gmail.com>
2026-02-28 11:53:29 +0300

    add new file lab1

---
+++ struct_1.py
@@ -0,0 +1,19 @@
+import  time
+
+
+A = int(input("Введите значение А в диапазоне от 2 до 2000000000 "))
+B = int(input("Введите значение B в диапазоне от 2 до 2000000000 "))
+
+start_time = time.time()
+
+n = 1
+for n in range(1, 100):
+    if B**n % A == 0:
+        print(n)
+        break
+else:
+    print(-1)
+
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"Время выполнения: {execution_time} секунд")
